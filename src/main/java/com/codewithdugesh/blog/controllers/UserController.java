cba735ec26aaab996633d8b2ccc3dcb1ed5ff80f
jerun276 <jerunkingston@gmail.com>
2024-02-14 10:14:29 +0530

    Validation Added

--- src/main/java/com/codewithdugesh/blog/controllers/UserController.java
+++ src/main/java/com/codewithdugesh/blog/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,14 +20,14 @@ public class UserController {
 
     //POST-create user
     @PostMapping("/")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
         UserDto createUserDto =this.userService.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
 
     //PUT-update user
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("userId") Integer uid) {
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto,@PathVariable("userId") Integer uid) {
         UserDto updatedUser = this.userService.updateUser(userDto,uid);
         return ResponseEntity.ok(updatedUser);
     }
--- src/main/java/com/codewithdugesh/blog/payloads/UserDto.java
+++ src/main/java/com/codewithdugesh/blog/payloads/UserDto.java
@@ -4,15 +4,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDto {
 
     private Integer id;
+
+    @NotEmpty
+    @Size(min = 4, message = "Username must be min of 4 characters !!")
     private String name;
+
+    @Email(message = "Email Address is not Valid")
     private String email;
+
+    @NotEmpty
+    @Size(min = 3, max = 10, message = "Password must be min of 3 char and max of 10 char !!")
     private String password;
+
+    @NotEmpty
     private String about;
 
 }
